3351c1bc00296cf4a5b6634abe20c70487278d99
Boatsremoval <serkormik@gmail.com>
2025-10-28 17:01:20 -0400

    Create cities.ts

---
+++ client/src/data/cities.ts
@@ -0,0 +1,30 @@
+export interface City {
+  name: string;
+  lat: number;
+  lon: number;
+}
+
+export const allCities: City[] = [
+  { name: "Pompano Beach", lat: 26.2379, lon: -80.1248 },
+  { name: "Fort Lauderdale", lat: 26.1224, lon: -80.1373 },
+  { name: "Deerfield Beach", lat: 26.3184, lon: -80.0996 },
+  { name: "Boca Raton", lat: 26.3683, lon: -80.1289 },
+  { name: "Coral Springs", lat: 26.2712, lon: -80.2706 },
+  { name: "Coconut Creek", lat: 26.2518, lon: -80.1789 },
+  { name: "Margate", lat: 26.2445, lon: -80.2064 },
+  { name: "Tamarac", lat: 26.2129, lon: -80.2498 },
+  { name: "Hollywood", lat: 26.0112, lon: -80.1495 },
+  { name: "Dania Beach", lat: 26.0523, lon: -80.1439 },
+  { name: "Pembroke Pines", lat: 26.0031, lon: -80.2239 },
+  { name: "Miramar", lat: 25.9873, lon: -80.2323 },
+  { name: "Weston", lat: 26.1004, lon: -80.3998 },
+  { name: "Delray Beach", lat: 26.4615, lon: -80.0728 },
+  { name: "Boynton Beach", lat: 26.5318, lon: -80.0905 },
+  { name: "Lake Worth Beach", lat: 26.6179, lon: -80.0720 },
+  { name: "Wellington", lat: 26.6618, lon: -80.2684 },
+  { name: "West Palm Beach", lat: 26.7153, lon: -80.0534 },
+  { name: "Miami Gardens", lat: 25.9420, lon: -80.2456 },
+  { name: "North Miami Beach", lat: 25.9331, lon: -80.1625 },
+  { name: "Miami", lat: 25.7617, lon: -80.1918 },
+  
+];
