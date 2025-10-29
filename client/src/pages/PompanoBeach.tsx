cc3df0af1c0b2882f845f1bcdb885dabf9d30105
Boatsremoval <serkormik@gmail.com>
2025-10-29 16:55:27 -0400

    Update PompanoBeach.tsx

--- client/src/pages/PompanoBeach.tsx
+++ client/src/pages/PompanoBeach.tsx
@@ -67,7 +67,7 @@ export default function PompanoBeach() {
         <meta property="og:type" content="website" />
         <link rel="canonical" href="https://boatsremoval.com/pompano-beach-boat-removal" />
       </Helmet>
-      <Header phone="954-398-5831" />
+      <Header />
       
       <main className="flex-1">
         {/* Hero Section */}
