6ef69e8cd463ebda0c9000f32e48b7f160de6e67
DimaGRA <d2011ng@hotmail.com>
2025-11-18 03:12:29 -0500

    Add Venice Gardens to cities data

--- client/src/data/cities.ts
+++ client/src/data/cities.ts
@@ -155,7 +155,8 @@ export const allCities: City[] = [
   { name: "Palmer Ranch", lat: 27.2706, lon: -82.4556, county: "Sarasota", phone: "727-298-0155"},
   { name: "Osprey", lat: 27.1588, lon: -82.4333, county: "Sarasota", phone: "727-298-0155"},
   { name: "Nokomis", lat: 27.1517, lon: -82.4092, county: "Sarasota" , phone: "727-298-0155"},
-  { name: "Venice", lat: 27.0998, lon: -82.4542, county: "Sarasota", phone: "727-298-0155"}
-  
+  { name: "Venice", lat: 27.0998, lon: -82.4542, county: "Sarasota", phone: "727-298-0155"},
+  { name: "Venice Gardens", lat: 27.0525, lon: -82.4319, county: "Sarasota", phone: "727-298-0155"}
+
 
 ];
