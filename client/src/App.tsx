5015471fe68dd72877d7f61c4b5036ab29141bba
Boatsremoval <serkormik@gmail.com>
2026-01-10 21:32:35 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import ScrollToTop from "@/components/ScrollToTop";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -438,6 +439,7 @@ function App() {
       <QueryClientProvider client={queryClient}>
         <TooltipProvider>
           <Toaster />
+          <ScrollToTop />
           <Router />
         </TooltipProvider>
       </QueryClientProvider>
