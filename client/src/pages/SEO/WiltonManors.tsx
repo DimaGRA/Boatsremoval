8449ae1ce677708cea9699a469fc786eb0d9e237
Boatsremoval <serkormik@gmail.com>
2026-07-26 17:36:02 -0400

    Update WiltonManors.tsx

--- client/src/pages/SEO/WiltonManors.tsx
+++ client/src/pages/SEO/WiltonManors.tsx
@@ -64,7 +64,7 @@ export default function WiltonManors() {
           content="Expert boat removal services in Wilton Manors, Florida. Free quotes, same-day service, eco-friendly disposal." 
         />
         <meta property="og:type" content="website" />
-        <link rel="canonical" href="https://boatsremoval.com/boat-removal-fl-wilton-manors" />
+        <link rel="canonical" href="https://www.boatsremoval.com/boat-removal-fl-wilton-manors" />
       </Helmet>
      <Header cityName="Wilton Manors" />
       
